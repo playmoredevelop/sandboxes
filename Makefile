ea5739482cf81385b76c0c1676f58f797a21cd68
playmore <playmoredevelop@gmail.com>
2021-10-21 13:31:14 +0300

    refactor configs path. add letzbrake example

--- Makefile
+++ Makefile
@@ -11,14 +11,14 @@ endef
 pixi-ts-gsap-spine:
 	@echo 'Sandbox with: PIXI.JS + Typescript + GSAP + Spine (Testing WebGL features)'
 	@touch ./src/index.ts
-	@${call init, ./configs/pixi/ts-gsap-spine.json}
+	@${call init, ./configs/ts-gsap-spine.json}
 
 pixi-ts-gsap-dragonbones:
 	@echo 'Sandbox with: PIXI.JS + Typescript + GSAP + DragonBones (Testing WebGL features)'
 	@touch ./src/index.ts
-	@${call init, ./configs/pixi/ts-gsap-dragonbones.json}
+	@${call init, ./configs/ts-gsap-dragonbones.json}
 
 js:
 	@echo 'Sandbox with: Vanilla Javascript + Decimal (initialized, please wait)'
 	@touch ./src/index.js
-	@${call init, ./configs/javascript/js-math.json}
+	@${call init, ./configs/js-math.json}
---
+++ resources/examples/letzbake.com.js
@@ -0,0 +1,30 @@
+/* eslint-disable require-jsdoc */
+const fetch = require('simple-fetch')
+
+const start = new Date().getTime()
+
+async function getBlockCommission(index) {
+
+    const block = await fetch.getJson('https://teznode.letzbake.com/chains/main/blocks/' + index)
+    const results = { count: 0, fee: 0, index, baker: block.metadata.baker }
+
+    block.operations.map(op => op.map(transaction => {
+
+        transaction.contents && transaction.contents.map(content => {
+            content.fee && (results.fee += parseFloat(content.fee))
+        }) && results.count ++
+    }))
+
+    return results
+}
+
+(async () => {
+    const results = await Promise.all([832543, 832544, 832545].map(getBlockCommission))
+    const total = results.reduce((a, c) => { return { count: a.count + c.count, fee: a.fee + c.fee } })
+    console.log('Count transactions', total.count)
+    console.log('Bakers fees total', total.fee)
+    console.log('Memory (heapUsed, MB)', Math.round(process.memoryUsage().heapUsed / 1024 / 1024 * 100) / 100)
+    console.log('Time (seconds)', (new Date().getTime() - start) / 1000)
+
+    console.table(results.map(v => ({ baker: v.baker, block: v.index, txCount: v.count, fee: v.fee })))
+})()
